fe681a34f57f1a5a8f5fcd03b0afecacd8813d7b
Gabriel J. Diaz <gabriel.diaz@rit.edu>
2015-11-05 09:15:47 -0500

    Dealing with merging issues.  UPdated main.py and gazetools.py

--- Vizard1.py
+++
@@ -1,8 +0,0 @@
-﻿import smi_beta
-
-def getGazePoint():# Get gaze matrix in local HMD coordinate system
-	gazeMat = gaze.getLastGazeMatrix()
-
-	# Transform gaze matrix into world coordinate system
-	gazeMat.postMult(viz.MainView.getMatrix())
-
